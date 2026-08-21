a0099089a51dcf04086a62e2c219fdb70b7ede82
CABP <cabp@carlos-bucheli.com>
2026-08-21 16:53:09 -0500

    CV Content - Education - Removing short version button
    Signed-off-by: CABP <cabp@carlos-bucheli.com>

--- src/components/molecules/detailPageEducation/DetailPageEducation.tsx
+++ src/components/molecules/detailPageEducation/DetailPageEducation.tsx
@@ -1,38 +1,92 @@
-import type { EducationInfoGroupsInterface } from '../../../app/services/CurriculumService.ts';
-import { capitalizeFirst } from '../../../app/utils/reactUtils.tsx';
-import FApp from '../../../app';
-
 function DetailPageEducation() {
-  const renderData = FApp.curriculumService.getEducationRenderData();
-
   return (
-    <div className="content text-dark w-100">
-      {
-        Object.keys(renderData).map((groupKey, i) => {
-          const group = renderData[groupKey as keyof EducationInfoGroupsInterface];
+    <div className="content text-dark pr-1 w-100">
+      <div className="info-group border-bottom w-100">
+        <h2>Formal</h2>
 
-          return (
-            <div key={ `key_ig_${ i }` } className="info-group">
-              <header>
-                <h2 className="m-0">{ capitalizeFirst(group.title) }</h2>
-              </header>
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Computer Sciences Bachelor, 1998</h3>
+          <p>Javeriano High School, Ecuador</p>
+        </div>
 
-              <div className="entry-list">
-                {
-                  group.data.map((entry, ii) => {
-                    return (
-                      <div key={ `key_if_${ ii }` } className="mb-2">
-                        <h3 className="m-0">{ entry.name }, { entry.endDate!.year }</h3>
-                        <p>{ entry.organization }, { entry.country.name }</p>
-                      </div>
-                    );
-                  })
-                }
-              </div>
-            </div>
-          );
-        })
-      }
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Systems &amp; Networking Engineer, 2004</h3>
+          <p>University Alfredo Pérez Guerrero, Ecuador</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Master of Business and Technology, 2021</h3>
+          <p>Universidad de San Andrés (UDESA), Argentina</p>
+        </div>
+      </div>
+
+      <div className="info-group border-bottom w-100">
+        <h2>Certifications</h2>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Project Management Professional (PMP), 2009</h3>
+          <p>Instituto Tecnológico y de Estudios Superiores de Monterrey (ITESM), Ecuador</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">International Software Testing Board (ISTQB), 2014</h3>
+          <p>Logic Studio, Ecuador</p>
+        </div>
+      </div>
+
+      <div className="info-group border-bottom w-100">
+        <h2>Courses &amp; Workshops</h2>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Cultural Intelligence, 2022 (course)</h3>
+          <p>Universidad de Palermo, Argentina</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Public Speaking, 2019 (course)</h3>
+          <p>Universidad de Palermo, Argentina</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Supply Chain Management, 2018 (workshop)</h3>
+          <p>Universidad de Palermo, Argentina</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">IFRS Cash Flow, 2011 (training)</h3>
+          <p>Conference, Training &amp; Events, Ecuador</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Formulating the Business Plan, 2008 (course)</h3>
+          <p>Quality Management Focus, Ecuador</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">English Business Meetings, 2008 (workshop)</h3>
+          <p>Key Language Services, Ecuador</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">Assessment Center, 2007 (workshop)</h3>
+          <p>Deloitte &amp; Touche, Ecuador</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">ITU EU Legal Aspects, 2007 (course)</h3>
+          <p>Netherlands Ministry of Foreign Affairs, Ecuador</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">CIT for Executives, 2007 (course)</h3>
+          <p>Corporaci&otilde;n Andina de Fomento, Ecuador</p>
+        </div>
+
+        <div className="fs-13 mb-1">
+          <h3 className="m-0">English Business Communications, 2006 (workshop)</h3>
+          <p>Key Language Services, Ecuador</p>
+        </div>
+      </div>
     </div>
   );
 }
